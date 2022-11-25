bcd59aa0658d844c728d6351df95ab8cf51df796
Steven Pears <steven.pears@live.co.uk>
2022-11-25 20:37:55 +0000

    Add Demo1 complete

--- ExampleRoslynDemos/Analyzer/Demo1.cs
+++ ExampleRoslynDemos/Analyzer/Demo1.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace ExampleRoslynDemos.Analyzer
 {
     internal class Demo1
     {
-
+       
     }
 }
